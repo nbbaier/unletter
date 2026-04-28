7cd8df48faf4be3a59e5eaf0c9f3b4596875a32c
Nico Baier <nico.baier@gmail.com>
2026-04-28 13:32:12 -0500

    Use ALCHEMY_PASSWORD from environment variable

--- scripts/log.ts
+++ scripts/log.ts
@@ -1,4 +1,4 @@
-// const ALCHEMY_PASSWORD = process.env.ALCHEMY_PASSWORD;
+const ALCHEMY_PASSWORD = process.env.ALCHEMY_PASSWORD;
 
 const result = await fetch(
   "https://nbbaier--8264a40c432d11f1b07842b51c65c3df.web.val.run",
@@ -8,7 +8,7 @@ const result = await fetch(
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      password: "test",
+      password: ALCHEMY_PASSWORD,
     }),
   }
 );
